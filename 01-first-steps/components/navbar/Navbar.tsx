e11e040287b2d18a085c185f0514d3bfee7d6585
Jhon Moofarrif <jhon.moofarrif@gmail.com>
2025-03-08 17:09:00 -0500

    feat: navbar

--- 01-first-steps/components/navbar/Navbar.tsx
+++ 01-first-steps/components/navbar/Navbar.tsx
@@ -1,22 +1,37 @@
+import Link from 'next/link';
+import { HomeIcon } from '@primer/octicons-react';
+import { ActiveLink } from '../active-link/ActiveLink';
+
+
+const navItems = [
+  { path: '/about', text: 'About' },
+  { path: '/pricing', text: 'Pricing' },
+  { path: '/contact', text: 'Contact' },
+]
+
 
 
 export const Navbar = () => {
-  console.log("first")
+
+  
+
   return (
-    <nav className="flex bg-blue-900 bg-opacity-90 p-2 m-2  rounded">
-      <a href="/" className="text-white hover:text-gray-200">
-        Home
-      </a>
+    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
+
+      <Link href={'/'} className='flex items-center'>
+        <HomeIcon className='mr-2' />
+        <span>Home</span>
+      </Link>
+
       <div className="flex flex-1"></div>
-      <a href="/pricing" className=" mr-3 text-white hover:text-gray-200">
-        Pricing
-      </a>
-      <a href="/contact" className=" mr-3 text-white hover:text-gray-200>">
-        Contact
-      </a>
-      <a href="/about" className=" mr-3 text-white hover:text-gray-200">
-        About
-      </a>
+
+      {
+        navItems.map( navItem => (
+          <ActiveLink key={navItem.path} { ...navItem }  />
+        ))
+      }
+
+
     </nav>
-  );
-};
+  )
+}
