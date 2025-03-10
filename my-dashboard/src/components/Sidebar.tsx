de5ace0076a772c328fee32233f09d227e362501
Jhon Moofarrif <jhon.moofarrif@gmail.com>
2025-03-10 08:48:25 -0500

    feat: text-white

--- my-dashboard/src/components/Sidebar.tsx
+++ my-dashboard/src/components/Sidebar.tsx
@@ -33,27 +33,26 @@ export const Sidebar = () => {
     <div
       id="menu"
       style={{ width: "25rem" }}
-      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64  left-0  overflow-y-scroll"
+      className="bg-gray-900 min-h-screen z-10 text-white w-64  left-0  overflow-y-scroll"
     >
       <div id="logo" className="my-4 px-6">
         <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
           <IoLogoReact className="mr-2" />
           <span>Dashboard</span>
         </h1>
-        <p className="text-slate-500 text-sm">
-          Manage your actions and activities
-        </p>
+        <p className="text-white text-sm">Manage your actions and activities</p>
       </div>
       <div id="profile" className="px-6 py-10">
-        <p className="text-slate-500">Welcome back,</p>
+        <p className="text-white">Welcome back,</p>
         <a href="#" className="inline-flex space-x-2 items-center">
           <span>
             <Image
               className="rounded-full w-8 h-8"
               src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c"
               alt="User avatar"
-              width={50}
-              height={50}
+              width={0}
+              height={0}
+              style={{ width: "32px", height: "auto" }}
             />
           </span>
           <span className="text-sm md:text-base font-bold">Jhon Moofarrif</span>
